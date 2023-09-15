efe4505217fc2838486ddd8836f12a768f857490
Али <lal4topol@yandex.ru>
2023-09-15 21:03:42 +0300

    Stopwatch v2 fixed a bug with multiple timer start. added hours. general work on optimization and logic has been carried out

--- Stopwatch/src/components/StopwatchItem.jsx
+++ Stopwatch/src/components/StopwatchItem.jsx
@@ -2,15 +2,66 @@ import playIcon from '../assets/icons/play.svg'
 import pauseIcon from '../assets/icons/pause.svg' 
 import repeatIcon from '../assets/icons/repeat.svg' 
 import closeIcon from '../assets/icons/close.svg' 
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 
 
 const StopwatchItem = ({obj,stopwatchList,setStopwatchList}) => {
 
-    let [time,setTime] = useState('00:00')
+    let currentTime
+    let [time,setTime] = useState('0:0:0')
+    let isPausedRef = useRef(false);
+    let isResetRef = useRef(false);
+    let isRunRef = useRef(false)
+    let [disabled, setDisabled] = useState(false)
+
+    function pauseTime() {
+        isPausedRef.current = true;
+        setDisabled(false)
+    }
+    function resetTime() {
+        isResetRef.current = true;
+        setTimeout(()=>{setTime('0:0:0')},100)
+        setDisabled(false)
+    }
+
 
     function runTime(event) {
-        let currentId = event.currentTarget.getAttribute('object-id')
+        setDisabled(true)
+        if(disabled===false) {
+            isRunRef.current = !isPausedRef.current;
+            isPausedRef.current = false;
+            isResetRef.current = false;
+    
+            currentTime = time.split(':')
+            currentTime = currentTime.map((elem)=> parseInt(elem))
+            const interval = setInterval(() => {
+    
+                if (isPausedRef.current === true) {
+                    clearInterval(interval);
+                }
+                
+                if(currentTime[2]<60) {
+                    currentTime[2] = currentTime[2]+1
+                }
+                if(currentTime[2]==60) {
+                    currentTime[1] = currentTime[1]+1
+                    currentTime[2] = 0
+                }
+                if(currentTime[1]==60) {
+                    currentTime[1] = 0
+                    currentTime[0] = currentTime[0]+1
+                }
+                let newTime = currentTime.join(':')
+                
+                if (isResetRef.current === true) {
+                    clearInterval(interval);
+                    newTime = ('0:0')
+                }
+                
+                setTime(newTime)
+            }, 1000);
+        }
+        
     }
 
     function deleteItem(event) {
@@ -34,9 +85,9 @@ const StopwatchItem = ({obj,stopwatchList,setStopwatchList}) => {
               </div>
               <h2>{time}</h2>
               <div className="buttom-side">
-                <button><img  src={playIcon} alt="play-button"  className="button-image" onClick={runTime}/></button>
-                <button><img src={pauseIcon} alt="pause-button"  className="button-image"/></button>
-                <button><img src={repeatIcon} alt="repeat-button"  className="button-image"/></button>
+                <button disabled={disabled}><img  src={playIcon} alt="play-button"  className="button-image" onClick={runTime}/></button>
+                <button><img src={pauseIcon} alt="pause-button"  className="button-image" onClick={pauseTime}/></button>
+                <button><img src={repeatIcon} alt="repeat-button"  className="button-image" onClick={resetTime}/></button>
               </div>
         </div>
     );
